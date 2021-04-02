ec5b7ff7a6d34218e8f24a24868c557f9fb7cbcb
theshashankk <78837537+theshashankk@users.noreply.github.com>
2021-04-02 16:19:13 +0530

    Update telesetup.py

--- telesetup.py
+++ telesetup.py
@@ -10,7 +10,9 @@ from telethon.sessions import StringSession
 print("""Please go-to my.telegram.org
 Login using your Telegram account
 Click on API Development Tools
-Create a new application, by entering the required details""")
+Create a new application, by entering the required details
+copyright;- [white devil](https://www.github.com/theshashank/whitedevil-x)
+""")
 APP_ID = int(input("Enter APP ID here: "))
 API_HASH = input("Enter API HASH here: ")
 
